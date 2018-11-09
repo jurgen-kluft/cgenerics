0c2ab7eff83d36e36b2544a9cd6009f49e21b8fd
Jurgen Kluft <jurgen.kluft@gmail.com>
2018-11-09 13:12:42 +0800

    brought into existence

---
+++ package/package.go
@@ -0,0 +1,37 @@
+package xgenerics
+
+import (
+	"github.com/jurgen-kluft/xbase/package"
+	"github.com/jurgen-kluft/xcode/denv"
+	"github.com/jurgen-kluft/xentry/package"
+	"github.com/jurgen-kluft/xunittest/package"
+)
+
+// GetPackage returns the package object of 'xgenerics'
+func GetPackage() *denv.Package {
+	// Dependencies
+	unittestpkg := xunittest.GetPackage()
+	entrypkg := xentry.GetPackage()
+	xbasepkg := xbase.GetPackage()
+
+	// The main (xgenerics) package
+	mainpkg := denv.NewPackage("xgenerics")
+	mainpkg.AddPackage(unittestpkg)
+	mainpkg.AddPackage(entrypkg)
+	mainpkg.AddPackage(xbasepkg)
+
+	// 'xgenerics' library
+	mainlib := denv.SetupDefaultCppLibProject("xgenerics", "github.com\\jurgen-kluft\\xgenerics")
+	mainlib.Dependencies = append(mainlib.Dependencies, xbasepkg.GetMainLib())
+
+	// 'xgenerics' unittest project
+	maintest := denv.SetupDefaultCppTestProject("xgenerics_test", "github.com\\jurgen-kluft\\xgenerics")
+	maintest.Dependencies = append(maintest.Dependencies, unittestpkg.GetMainLib())
+	maintest.Dependencies = append(maintest.Dependencies, entrypkg.GetMainLib())
+	maintest.Dependencies = append(maintest.Dependencies, xbasepkg.GetMainLib())
+	maintest.Dependencies = append(maintest.Dependencies, mainlib)
+
+	mainpkg.AddMainLib(mainlib)
+	mainpkg.AddUnittest(maintest)
+	return mainpkg
+}
