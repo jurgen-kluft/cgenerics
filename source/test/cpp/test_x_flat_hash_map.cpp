49fdbf3f99a5f5cfc5fe1ac79c83fe7fb2518720
Jurgen Kluft <jurgen@virtuosgames.com>
2021-12-29 14:24:19 +0800

    * flat hashmap; fix compilation issues

--- source/test/cpp/test_x_flat_hash_map.cpp
+++ source/test/cpp/test_x_flat_hash_map.cpp
@@ -28,10 +28,64 @@ UNITTEST_SUITE_BEGIN(flat_hashmap)
                 {
                     ctrl.set_hash((u8)h, p);
                     u32 match = ctrl.match((u8)h, 0xffffffff);
-                    CHECK_EQUAL((u32)(1<<p), match);
+                    CHECK_EQUAL((u32)(1 << p), match);
                     ctrl.clr_hash(p);
                 }
             }
+        }
+        UNITTEST_TEST(group_empty)
+        {
+            flat_hashmap_n::group_t group;
+            group.clear();
+
+            CHECK_EQUAL(true, group.has_empty());
+            CHECK_EQUAL(false, group.has_deleted());
+
+            for (s32 i = 0; i < 32; ++i)
+            {
+                CHECK_EQUAL(true, group.is_empty(i));
+            }
+            for (s32 i = 0; i < 32; ++i)
+            {
+                CHECK_EQUAL(false, group.is_deleted(i));
+            }
+        }
+
+        UNITTEST_TEST(group_test_empty)
+        {
+            flat_hashmap_n::group_t group;
+            group.clear();
+
+            CHECK_EQUAL(true, group.has_empty());
+            CHECK_EQUAL(false, group.has_deleted());
+
+            for (s32 i = 0; i < 32; ++i)
+            {
+                group.set_empty(i);
+                CHECK_EQUAL(true, group.is_empty(i));
+            }
+        }
+
+        UNITTEST_TEST(group_test_deleted)
+        {
+            flat_hashmap_n::group_t group;
+            group.clear();
+
+            CHECK_EQUAL(true, group.has_empty());
+            CHECK_EQUAL(false, group.has_deleted());
+
+            for (s32 i = 0; i < 32; ++i)
+            {
+                group.set_deleted(i);
+                CHECK_EQUAL(true, group.is_deleted(i));
+            }
+            CHECK_EQUAL(true, group.has_deleted());
+        }
+
+        UNITTEST_TEST(insert)
+        {
+            flat_hashmap_n::hashmap_t<s32, s32> map;
+            map.insert(0, 0);
         }
     }
 }
