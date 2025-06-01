cacc3215e73703753358cf202b50ba8bfef02ba7
jurgen.kluft <jurgen.kluft@gmail.com>
2025-06-01 08:32:58 +0800

    * adjust to changes in ccode

--- package/package.go
+++ package/package.go
@@ -13,19 +13,19 @@ func GetPackage() *denv.Package {
 	cbasepkg := cbase.GetPackage()
 
 	// The main (cgenerics) package
-	mainpkg := denv.NewPackage("cgenerics")
+	mainpkg := denv.NewPackage("github.com\\jurgen-kluft", "cgenerics")
 	mainpkg.AddPackage(unittestpkg)
 	mainpkg.AddPackage(cbasepkg)
 
 	// 'cgenerics' library
-	mainlib := denv.SetupCppLibProject("cgenerics", "github.com\\jurgen-kluft\\cgenerics")
+	mainlib := denv.SetupCppLibProject(mainpkg, "cgenerics")
 	mainlib.AddDependencies(cbasepkg.GetMainLib()...)
 
 	// 'cgenerics' unittest project
-	maintest := denv.SetupDefaultCppTestProject("cgenerics_test", "github.com\\jurgen-kluft\\cgenerics")
+	maintest := denv.SetupCppTestProject(mainpkg, "cgenerics_test")
 	maintest.AddDependencies(unittestpkg.GetMainLib()...)
 	maintest.AddDependencies(cbasepkg.GetMainLib()...)
-	maintest.Dependencies = append(maintest.Dependencies, mainlib)
+	maintest.AddDependency(mainlib)
 
 	mainpkg.AddMainLib(mainlib)
 	mainpkg.AddUnittest(maintest)
